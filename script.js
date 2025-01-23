4f25fdc6b3f5a13538e983be2938e519a74dd854
Danikchik <danik333904@gmail.com>
2025-01-23 19:00:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/*первое задание*/
+
+const userInputName = prompt('Ваше имя')
+console.log(userInputName)
+const userInputAge = prompt('Ваш возраст')
+console.log(userInputAge)
+const userInputAdd = prompt('2+2?')
+console.log(userInputAdd)
+const userInputSub = prompt('40-10?')
+console.log(userInputSub)
+const userInputShare = prompt('30/2?')
+console.log(userInputShare)
+const userInputMul = prompt('10*10?')
+console.log(userInputMul)
+const userInputRes = prompt('10%3?')
+console.log(userInputRes)
+alert('посмотрите в консоль')
+
+/*второе задание*/
+
+
+const num1 = prompt("Введите первое число:");
+const num2 = prompt("Введите второе число:");
+const num3 = prompt("Введите третье число:");
+
+
+const res = (num1 + num2 + num3)/3
+console.log(`Среднее число: ${res}`);
+
